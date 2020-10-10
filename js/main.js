3ba76d4b86fb628931ea61e76d5b3c9313157f79
Andrey Markov <markov@workmail.com>
2020-10-10 21:18:17 +0300

    Removed GA, added log for main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
+console.log("Loading main.js");
+
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('../sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful: ', registration);
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('../sw.js').then(function (registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful: ', registration);
+    }, function (err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
     });
-  }
+  });
+}
+
+console.log("Done loading main.js");
