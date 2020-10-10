067efa6cc632caee6c7655621640ce6a1bb44d73
Andrey Markov <markov@workmail.com>
2020-10-10 21:34:42 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-  '/',
-  '/css/main.css',
-  '/js/main.js'
+  'css/main.css',
+  'js/main.js'
 ];
 
 self.addEventListener('install', function(event) {
